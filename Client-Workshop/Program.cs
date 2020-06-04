db352f477c4014b7a330c7eb240fc0b1fb66a23f
Julien Juncker <juncker.juncker@viacesi.fr>
2020-06-04 14:38:47 +0200

    generate client

---
+++ Client-Workshop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculationClient client = new CalculationClient();
+            int i = 2, j = 3;
+            int result = client.m_add(i, j);
+            Console.WriteLine(result);
+
+            client.Close();
+        }
+    }
+}
